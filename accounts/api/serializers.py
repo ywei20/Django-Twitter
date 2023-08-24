0d6cd7056db8a4143583397db8f4310443c7e074
ywei <weiyanyoyo@gmail.com>
2023-08-24 16:09:02 -0700

    login_status, logout, login, signup API

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,56 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework import exceptions
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email']
+        fields = ['username', 'email']
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        # check if user exists
+        if not User.objects.filter(username=attrs['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'username': "User does not exist."
+            })
+        return attrs
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def validate(self, attrs):
+        # This method will be called by serializer.is_valid()
+        if User.objects.filter(username=attrs['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'username': 'This username has been occupied.'
+            })
+        if User.objects.filter(username=attrs['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'email': 'This email has been occupied.'
+            })
+        return attrs
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+        )
+        return user
